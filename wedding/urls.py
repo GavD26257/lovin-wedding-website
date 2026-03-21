fd7bb4832e59f36fe681e342136f1ed42ba903a7
gavd26257 <u0982767@utah.edu>
2026-03-21 15:43:07 -0600

    Adjusted sections (added Faqs, removed activity suggestions, etc.), changed first several photos, changed event and story text, changed fonts

--- wedding/urls.py
+++ wedding/urls.py
@@ -1,7 +1,10 @@
 from django.urls import re_path
+from django.views.generic.base import RedirectView
 
 from . import views
 
 urlpatterns = [
-    re_path(r'^$', views.home, name='home'),
+    re_path(r'^$', RedirectView.as_view(url='reception/', permanent=True)),
+    re_path(r'^reception/$', views.reception, name='reception'),
+    re_path(r'^ceremony/$', views.ceremony, name='ceremony'),
 ]
--- wedding/views.py
+++ wedding/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from guests.save_the_date import SAVE_THE_DATE_CONTEXT_MAP
 
 
+#TODO try commenting out this view
 def home(request):
     return render(request, 'home.html', context={
         'save_the_dates': SAVE_THE_DATE_CONTEXT_MAP,
@@ -11,4 +12,26 @@ def home(request):
         'couple_name': settings.BRIDE_AND_GROOM,
         'wedding_location': settings.WEDDING_LOCATION,
         'wedding_date': settings.WEDDING_DATE,
+    })
+
+def ceremony(request):
+    return render(request, 'home.html', context={
+        'save_the_dates': SAVE_THE_DATE_CONTEXT_MAP,
+        'support_email': settings.DEFAULT_WEDDING_REPLY_EMAIL,
+        'website_url': settings.WEDDING_WEBSITE_URL,
+        'couple_name': settings.BRIDE_AND_GROOM,
+        'wedding_location': settings.WEDDING_LOCATION,
+        'wedding_date': settings.WEDDING_DATE,
+        'ceremony': True,
+    })
+
+def reception(request):
+    return render(request, 'home.html', context={
+        'save_the_dates': SAVE_THE_DATE_CONTEXT_MAP,
+        'support_email': settings.DEFAULT_WEDDING_REPLY_EMAIL,
+        'website_url': settings.WEDDING_WEBSITE_URL,
+        'couple_name': settings.BRIDE_AND_GROOM,
+        'wedding_location': settings.WEDDING_LOCATION,
+        'wedding_date': settings.WEDDING_DATE,
+        'ceremony': False,
     })
